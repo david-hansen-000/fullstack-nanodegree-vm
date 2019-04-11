96aa72fa7095b8e01ecde7ea8d8fd6b27f35c35d
david <david.hansen.0000@gmail.com>
2019-04-10 23:31:11 -0600

    added to file to make it more usable

--- vagrant/catalog/flask_webserver.py
+++ vagrant/catalog/flask_webserver.py
@@ -2,7 +2,7 @@ from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from catalog.database_setup import Base, MenuItem, Restaurant
+from database_setup import Base, MenuItem, Restaurant
 
 app = Flask(__name__)
 
@@ -16,8 +16,19 @@ session = DBSession()
 
 @app.route('/')
 def hello_restaurants():
-    restaurant = session.query(Restaurant).first()
-    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = "<html><body>"
+    restaurants = session.query(Restaurant).all()
+    for restaurant in restaurants:
+        output += "<h1>{}</h1>".format(restaurant.name)
+        output += "<h3>Menu Items</h3>"
+        items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+        for item in items:
+            output += "<br>Item Name: {}".format(item.name)
+            output += "<br>Descriptioin: {}".format(item.description)
+            output += "<br>Price: {}".format(item.price)
+        output += "<br><br>"
+    output += "</body></html>"
+                                                
 
 
 if __name__ == '__main__':
